39926ebcb61adf00bdb52aa733adfba94e75b676
Prajwal Tuladhar <praj@infynyxx.com>
2020-01-23 23:40:29 -0500

    checkstyles and formatting

--- grpc-todo/src/main/java/grpc/todo/TodoServer.java
+++ grpc-todo/src/main/java/grpc/todo/TodoServer.java
@@ -7,32 +7,34 @@ import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
 public class TodoServer {
-    private static final Logger logger = Logger.getLogger(TodoServer.class.getName());
+  private static final Logger logger = Logger.getLogger(TodoServer.class.getName());
 
-    final int port;
+  final int port;
 
-    public TodoServer(int port) {
-        this.port = port;
-    }
+  public TodoServer(int port) {
+    this.port = port;
+  }
 
-    void start() throws IOException, InterruptedException {
-        final Server server = ServerBuilder
-                .forPort(port)
-                .addService(new TodoServiceGrpcImpl()).build().start();
-        logger.info(String.format("Started server at port=%d", port));
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try {
-                server.shutdown().awaitTermination(30, TimeUnit.SECONDS);
-            } catch (Exception e) {
-                System.err.println("Unable to shutdown server");
-                e.printStackTrace();
-            }
-        }));
-        server.awaitTermination();
-    }
+  void start() throws IOException, InterruptedException {
+    final Server server =
+        ServerBuilder.forPort(port).addService(new TodoServiceGrpcImpl()).build().start();
+    logger.info(String.format("Started server at port=%d", port));
+    Runtime.getRuntime()
+        .addShutdownHook(
+            new Thread(
+                () -> {
+                  try {
+                    server.shutdown().awaitTermination(30, TimeUnit.SECONDS);
+                  } catch (Exception e) {
+                    System.err.println("Unable to shutdown server");
+                    e.printStackTrace();
+                  }
+                }));
+    server.awaitTermination();
+  }
 
-    public static void main(String[] args) throws IOException, InterruptedException {
-        final TodoServer server = new TodoServer(5001);
-        server.start();
-    }
+  public static void main(String[] args) throws IOException, InterruptedException {
+    final TodoServer server = new TodoServer(5001);
+    server.start();
+  }
 }
--- grpc-todo/src/main/java/grpc/todo/TodoServiceGrpcImpl.java
+++ grpc-todo/src/main/java/grpc/todo/TodoServiceGrpcImpl.java
@@ -13,43 +13,40 @@ import todos.Todos.Todo;
 
 public class TodoServiceGrpcImpl extends TodoServiceGrpc.TodoServiceImplBase {
 
-    private final Map<Integer, Todo> map = Maps.newHashMap();
+  private final Map<Integer, Todo> map = Maps.newHashMap();
 
-    @Override
-    public void getTodoById(GetTodoRequest request, StreamObserver<Todo> responseObserver) {
-        final Todo todo = map.get(request.getId());
-        if (todo == null) {
-            responseObserver.onError(Status
-                    .NOT_FOUND
-                    .withDescription("id not present for updating")
-                    .asRuntimeException());
-        } else {
-            responseObserver.onNext(todo);
-        }
-        responseObserver.onCompleted();
+  @Override
+  public void getTodoById(GetTodoRequest request, StreamObserver<Todo> responseObserver) {
+    final Todo todo = map.get(request.getId());
+    if (todo == null) {
+      responseObserver.onError(
+          Status.NOT_FOUND.withDescription("id not present for updating").asRuntimeException());
+    } else {
+      responseObserver.onNext(todo);
     }
+    responseObserver.onCompleted();
+  }
 
-    @Override
-    public void updateTodo(Todo request, StreamObserver<Todo> responseObserver) {
-        if (request.getId() <= 0 || !map.containsKey(request.getId())) {
-            responseObserver.onError(Status
-                    .NOT_FOUND
-                    .withDescription(String.format("id=%d not present for updating",
-                            request.getId()))
-                    .asRuntimeException());
-        } else {
-            map.put(request.getId(), request);
-            responseObserver.onNext(request);
-        }
-        responseObserver.onCompleted();
+  @Override
+  public void updateTodo(Todo request, StreamObserver<Todo> responseObserver) {
+    if (request.getId() <= 0 || !map.containsKey(request.getId())) {
+      responseObserver.onError(
+          Status.NOT_FOUND
+              .withDescription(String.format("id=%d not present for updating", request.getId()))
+              .asRuntimeException());
+    } else {
+      map.put(request.getId(), request);
+      responseObserver.onNext(request);
     }
+    responseObserver.onCompleted();
+  }
 
-    @Override
-    public void createTodo(Todo request, StreamObserver<Todo> responseObserver) {
-        final int id = TodoClient.randomUnsignedInt();
-        final Todo todoWithId = request.toBuilder().setId(id).build();
-        map.put(id, todoWithId);
-        responseObserver.onNext(todoWithId);
-        responseObserver.onCompleted();
-    }
+  @Override
+  public void createTodo(Todo request, StreamObserver<Todo> responseObserver) {
+    final int id = TodoClient.randomUnsignedInt();
+    final Todo todoWithId = request.toBuilder().setId(id).build();
+    map.put(id, todoWithId);
+    responseObserver.onNext(todoWithId);
+    responseObserver.onCompleted();
+  }
 }
