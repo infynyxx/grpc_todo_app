17e8787b5a93033738fd53e6224ab2a82e8ca578
Prajwal Tuladhar <praj@infynyxx.com>
2021-09-09 23:08:03 -0400

    fix build

--- client/src/styles/variables.js
+++ client/src/styles/variables.js
@@ -1,7 +1,9 @@
-export default {
+const colors =  {
     black: '#000000',
     gray400: '#9B9B9B',
     white: '#FFFFFF',
     lime: '#cddc39',
     lightGren: '#e8f5e9'
 }
+
+export default colors;
