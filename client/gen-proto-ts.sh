19a86ed1148d722c2b6464e9cb098fb223648fb1
Prajwal Tuladhar <praj@infynyxx.com>
2025-09-30 22:38:28 -0400

    fix sed syntax

--- client/gen-proto-ts.sh
+++ client/gen-proto-ts.sh
@@ -13,17 +13,30 @@ echo "Compiling protobuf definitions..."
 for f in ./src/pb_generated/todos/*.js
 do
     echo '/* eslint-disable */' | cat - "${f}" > temp && mv temp "${f}"
+    
+    # Detect OS for sed compatibility
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        SED_INPLACE="sed -i ''"
+    else
+        SED_INPLACE="sed -i"
+    fi
+    
     # Convert require statements to imports
-    sed -i '' 's/var \([^=]*\) = require(\([^)]*\));/import * as \1 from \2;/g' "${f}"
+    $SED_INPLACE "s/var \([^=]*\) = require(\([^)]*\));/import * as \1 from \2;/g" "${f}"
+    
     # Fix specific grpc import
-    sed -i '' 's/var grpc = require("@improbable-eng\/grpc-web").grpc;/import { grpc } from "@improbable-eng\/grpc-web";/g' "${f}"
+    $SED_INPLACE "s/var grpc = require(\"@improbable-eng\/grpc-web\").grpc;/import { grpc } from \"@improbable-eng\/grpc-web\";/g" "${f}"
 
     # Handle protobuf files specifically
     if [[ "$f" == *"todos_pb.js" ]]; then
         # Replace goog.object.extend(exports, proto.todos.v1); with specific exports
-        sed -i '' 's/goog.object.extend(exports, proto.todos.v1);/export const Todo = proto.todos.v1.Todo;\nexport const GetTodoRequest = proto.todos.v1.GetTodoRequest;\nexport const DeleteTodoRequest = proto.todos.v1.DeleteTodoRequest;\nexport const DeleteTodoResponse = proto.todos.v1.DeleteTodoResponse;\nexport const ListTodoRequest = proto.todos.v1.ListTodoRequest;/g' "${f}"
+        $SED_INPLACE "s/goog.object.extend(exports, proto.todos.v1);/export const Todo = proto.todos.v1.Todo;\\
+export const GetTodoRequest = proto.todos.v1.GetTodoRequest;\\
+export const DeleteTodoRequest = proto.todos.v1.DeleteTodoRequest;\\
+export const DeleteTodoResponse = proto.todos.v1.DeleteTodoResponse;\\
+export const ListTodoRequest = proto.todos.v1.ListTodoRequest;/g" "${f}"
     else
         # Convert exports.X = Y; to export { Y as X };
-        sed -i '' 's/exports\.\([^=]*\) = \([^;]*\);/export { \2 as \1 };/g' "${f}"
+        $SED_INPLACE "s/exports\.\([^=]*\) = \([^;]*\);/export { \2 as \1 };/g" "${f}"
     fi
 done
